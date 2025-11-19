32f7e891c841baabd43ec597e6f233482b98c4de
marrodcab12 <marrodcab12@alum.us.es>
2025-11-19 14:06:44 +0100

    Update deployment workflow and improve health check response format

--- src/apis/v1.ts
+++ src/apis/v1.ts
@@ -4,14 +4,18 @@ function loadV1(app: Hono) {
   app.get('/v1', (c) => {
   return c.text('Api Gateway 0debt')
 })
+
 app.get('/v1/health', (ctx) => {
     return ctx.json({
       message: 'API GATEWAY',
-      version: 1,
+      version: '1',
       status: 'healthy',
       timestamp:new Date().toISOString()
     })
   })
+
+
+
 }
 
 
